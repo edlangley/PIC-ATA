f27dd3f2401baeb7504fbcf5ff840326c70dc473
Ed Langley <ed@edlangley.co.uk>
2015-01-01 01:57:23 +0000

    Make the return status of ATA functions that is printed to the serial port consistent

--- demo/main.c
+++ demo/main.c
@@ -15,50 +15,53 @@ Description.. Main code to operate an ATA interface and FAT32 FS on
 void main(void)
 {
     int8_t retval;
+    uint16_t wordix, sectorword;
 
     InitComms();
 
-    printf("PIC ATA interface test\n\r");
+    printf("\n\rPIC ATA interface test\n\r");
 
     printf("Init ATA: ");
-    retval = ATA_Init(ATA_MASTER);
-    switch(retval)
+    if((retval = ATA_Init(ATA_MASTER)) != ATA_OK)
     {
-    case ATA_OK:
-        printf("ATA master initialised\n\r");
-        break;
-    case ATA_RDY_TIMEOUT:
-        printf("ATA RDY timeout\n\r");
-        break;
-    case ATA_BSY_TIMEOUT:
-        printf("ATA BSY timeout\n\r");
-        break;
-    case ATA_DRQ_TIMEOUT:
-        printf("ATA DRQ timeout\n\r");
-        break;
+        printf("ATA error %d\n\r", retval);
+    }
+    else
+    {
+        printf("OK\n\r");
+    }
+
+    // DEBUG: dump the first sector
+    printf("Reading sector 0: ");
+    if((retval = ATA_SetLBAForRead(0)) != ATA_OK)
+    {
+        printf("ATA error %d\n\r", retval);
+    }
+    else
+    {
+        printf("LBA set\n\r");
+    }
+    for(wordix = 0; wordix < 256; wordix++)
+    {
+        sectorword = ATA_ReadWord();
+        printf("\tword %3d: 0x%04X, %c%c\n\r", wordix, sectorword, (sectorword >> 8), (sectorword & 0x00FF));
     }
 
 #if defined(ATA_USE_ID)
     printf("Read drive info: ");
-    retval = ATA_ReadDriveInfo();
-
-    switch(retval)
+    if((retval = ATA_ReadDriveInfo()) != ATA_OK)
     {
-    case ATA_OK:
-        printf("\n\r\tDrive model: %s\n\r\tRevision: %s\n\r\tSerial #:%s\n\r\tCylinders: %ud\n\r\tHeads: %ud\n\r\tSectors: %ud\n\r",
-        ATA_GetInfoModel(),
-        ATA_GetInfoRev(),
-        ATA_GetInfoSerialNum(),
-        ATA_GetInfoNumCyls(),
-        ATA_GetInfoNumHeads(),
-        ATA_GetInfoNumSectors());
-        break;
-    case ATA_BSY_TIMEOUT:
-        printf("ATA BSY timeout\n\r");
-        break;
-    case ATA_DRQ_TIMEOUT:
-        printf("ATA DRQ timeout\n\r");
-        break;
+        printf("ATA error %d\n\r", retval);
+    }
+    else
+    {
+        printf("\n\r\tDrive model: %s\n\r\tRevision: %s\n\r\tSerial #:%s\n\r\tCylinders: %u\n\r\tHeads: %u\n\r\tSectors: %u\n\r",
+                ATA_GetInfoModel(),
+                ATA_GetInfoRev(),
+                ATA_GetInfoSerialNum(),
+                ATA_GetInfoNumCyls(),
+                ATA_GetInfoNumHeads(),
+                ATA_GetInfoNumSectors());
     }
 #endif
 
