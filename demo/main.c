a0e09eacd60313a3bcaecf92253f05160f1d466b
Ed Langley <ed@edlangley.co.uk>
2015-01-14 00:17:33 +0000

    Open file hello.txt in root of FAT32 FS and print contents (ASCII assumed) Tidy up demo output.

--- demo/main.c
+++ demo/main.c
@@ -45,12 +45,12 @@ void main(void)
     for(byteix = 0; byteix < 512; byteix++)
     {
         sectorbyte = ATA_ReadSectorByte();
-        if((!(byteix % 16)) && (byteix != 0))
+        if(!(byteix % 16))
             printf("\n\r%03X: ", byteix);
         printf(" %02X", sectorbyte);
     }
 
-    // DEBUG:
+    // DEBUG: dump the BPB of the FAT32 FS
     printf("\n\r\n\rReading sector 0x00004ABC: ");
     if((retval = ATA_SetSectorLBAForRead(0x00004ABC)) != ATA_OK)
     {
@@ -63,7 +63,7 @@ void main(void)
     for(byteix = 0; byteix < 512; byteix++)
     {
         sectorbyte = ATA_ReadSectorByte();
-        if((!(byteix % 16)) && (byteix != 0))
+        if(!(byteix % 16))
             printf("\n\r%03X: ", byteix);
         printf(" %02X", sectorbyte);
     }
@@ -87,19 +87,23 @@ void main(void)
 #endif
 
 #ifdef FAT32_ENABLED
-    printf("\n\r\n\rMount FAT32: ");
+    printf("\n\rMount FAT32: ");
     if((retval = FAT32_Mount(0)) != FAT32_OK)
     {
         printf("Error %d\n\r", retval);
     }
     else
     {
+        FD testfd;
+        uint8_t databyte;
+
         printf("OK\n\r");
 
         printf("Root dir contents:\n\r");
-        while((retval = FAT32_DirLoadNextEntry()) == FAT32_OK)
+        retval = FAT32_DirLoadNextEntry();
+        while((retval == FAT32_OK) || (retval == FAT32_DIRENTRY_IS_DIR))
         {
-            printf("%s", FAT32_DirEntryName());
+            printf("%-12s", FAT32_DirEntryName());
             if(retval == FAT32_DIRENTRY_IS_DIR)
             {
                 printf("\t<dir>\n\r");
@@ -108,11 +112,41 @@ void main(void)
             {
                 printf("\n\r");
             }
+            retval = FAT32_DirLoadNextEntry();
         }
         if(retval != FAT32_EODIRENTRYS)
         {
             printf("Error %d loading next dir record\n\r", retval);
         }
+
+        printf("\n\rRe-opening root dir: ");
+        if((retval = FAT32_DirOpen("\\")) != FAT32_OK)
+        {
+            printf("Error %d\n\r", retval);
+        }
+        else
+        {
+            printf("OK\n\r");
+        }
+
+        printf("\n\rOpening file HELLO.TXT: ");
+        if((retval = FAT32_FileOpen(&testfd, "HELLO.TXT")) != FAT32_OK)
+        {
+            printf("Error %d\n\r", retval);
+        }
+        else
+        {
+            printf("OK\n\r");
+        }
+
+        printf("\n\rPrinting file HELLO.TXT:\n\r");
+        retval = FAT32_FileRead(&testfd, 1, &databyte);
+        while(retval == FAT32_OK)
+        {
+            printf("%c", databyte);
+            retval = FAT32_FileRead(&testfd, 1, &databyte);
+        }
+        printf("\n\r");
     }
 #endif
 }
